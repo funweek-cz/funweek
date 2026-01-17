6f8ca8869efee5a320661af038c918fc2afde7f1
savycek <me@savycek.eu>
2026-01-17 17:02:44 +0100

    pre-deploy empty commit (no changes made)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="h-100 md:h-150 max-w-7xl flex flex-col items-center justify-center bg-funweek text-white bg-cover bg-center text-5xl font-bold relative mx-auto my-10 mt-23 rounded-4xl">
         #fw28
         <span className="text-sm text-center">
-          pokud tu jsi, tak jsi jedinečný... a taky extrémně brzy.
+          pokud tu jsi, tak jsi jedinečný... a taky extrémně brzy!
         </span>
       </div>
     </main>
